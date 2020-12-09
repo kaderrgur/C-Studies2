321607564d9d234e35a6c0c8aa3b508fe25c86b4
Kader Gür <kadergur236@gmail.com>
2020-12-09 21:24:02 +0300

    All Project

---
+++ project 7/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace project_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+            int soruno = 0, dogru = 0, yanlis = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+
+            label8.Text = button1.Text;
+            if (label7 == label8)
+            {
+                dogru++;
+                label4.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                label6.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+
+
+
+        }
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = false;
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+
+
+            soruno++;
+            label2.Text = soruno.ToString();
+
+            if (soruno == 1)
+            {
+                richTextBox1.Text = "Cumhuriyet kaç yılında ilan edilmiştir?";
+                button1.Text = "1920";
+                button2.Text = "1921";
+                button3.Text = "1922";
+                button4.Text = "1923";
+                label7.Text = "1923";
+            }
+            if (soruno == 2)
+            {
+                richTextBox1.Text = "Hangi il Marmara bölgemizde değilidir?";
+                button1.Text = "Manisa";
+                button2.Text = "Bursa";
+                button3.Text = "Edirne";
+                button4.Text = "Kırklareli";
+                label7.Text = "Manisa";
+            }
+            if (soruno == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Text = "Sonuçlar";
+                MessageBox.Show("Dogru:  " + dogru + "\n" + "Yanlış:  " + yanlis);
+            }
+
+
+
+                
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+
+            label8.Text = button2.Text;
+            if (label7 == label8)
+            {
+                dogru++;
+                label4.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                label6.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+
+            label8.Text = button3.Text;
+            if (label7 == label8)
+            {
+                dogru++;
+                label4.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                label6.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+
+            label8.Text = button4.Text;
+            if (label7 == label8)
+            {
+                dogru++;
+                label4.Text = dogru.ToString();
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                yanlis++;
+                label6.Text = yanlis.ToString();
+                pictureBox2.Visible = true;
+            }
+        }
+    }
+}
